2d5b49f61c799fce5659959e9367d23abc06487c
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2019-03-29 19:21:52 -0500

    Create gcd_euclid.cpp

---
+++ src/math_problems/gcd_euclid.cpp
@@ -0,0 +1,36 @@
+//g++  5.4.0
+//GCD OF TWO NUMBERS x&Y. Euclidean Algorithm
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x=15;
+    int y=5;
+    
+    
+    int a;
+    int b;
+    int r;
+    
+    if(x>y)
+    {
+        a=x;
+        b=y;
+    }
+    else
+    {
+        b=x;
+        a=y;
+    }
+    
+    while(b!=0)
+    {
+        r=a%b;
+        a=b;
+        b=r;
+        
+    }
+    cout<<"\nGCD is "<<a;
+    return 0;
+    
+}

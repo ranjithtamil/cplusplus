f8e92422b71894713c0014a73be29e7a1c550f2c
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2019-12-22 11:57:31 -0600

    Create majorityElement.cpp

---
+++ src/LeetCode/majorityElement.cpp
@@ -0,0 +1,29 @@
+/*
+169. Majority Element
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int limit=n/2;
+        unordered_map<int,int> hist;
+        for(int i=0;i<n;i++) {
+            if(++hist[nums[i]] > limit) {
+                return nums[i];
+            }   
+        }
+        return -1;
+    }
+};

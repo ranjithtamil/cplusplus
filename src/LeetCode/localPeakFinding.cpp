c04eca7c27e62ab0fbc610f637cb678ed462cb70
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2019-12-22 11:41:38 -0600

    Create localPeakFinding.cpp

---
+++ src/LeetCode/localPeakFinding.cpp
@@ -0,0 +1,63 @@
+//162. Find Peak Element
+/*
+A peak element is an element that is greater than its neighbors.
+
+Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 1 or 5 
+Explanation: Your function can return either index number 1 where the peak element is 2, 
+             or index number 5 where the peak element is 6.
+Note:
+
+Your solution should be in logarithmic complexity.
+*/
+
+class Solution {
+public:
+    int bSearch(vector<int>& nums, int low, int high) {
+        //ITERATIVE
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if(low==high) {
+                return low;
+            }
+            if(nums[mid] < nums[mid+1]) {
+                low=mid+1;
+            }
+            else {
+                high=mid;
+            }
+        }
+        return low;
+        /* RECURSIVE
+            int mid=(low+high)/2;
+            if(low==high) {
+                return low;
+            }
+            if(nums[mid]< nums[mid+1]) {
+                return bSearch(nums, mid+1, high);
+            }
+            else {
+                return bSearch(nums, low, mid);
+            }
+        */
+        
+    }
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int idx=bSearch(nums,0,n-1);
+        return idx;
+    }
+};

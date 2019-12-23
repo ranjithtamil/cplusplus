b1a7a504cfd226f56497e3fb8e92f5278bf491a6
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2019-12-22 21:01:19 -0600

    Update selectionSortRevise.cpp

--- src/sort/selectionSortRevise.cpp
+++ src/sort/selectionSortRevise.cpp
@@ -20,12 +20,16 @@ int main()
     
     for(int i=0;i<n-1;i++) {
         int minidx=i;
+        int flag=0;
         for(int j=i+1;j<n;j++) {
             if(arr[j]<arr[minidx]) {
+                flag=1;
                 minidx=j;
             }
         }
-        swap(arr[i],arr[minidx]);
+        if(flag==1) {
+            swap(arr[i],arr[minidx]);
+        }
     }
     printArray(arr,n);
     return 0;

73f77d5a9180d201ec617ba7bf5a41d3f682554d
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2020-01-05 23:03:53 -0600

    Update BTInorder.cpp

--- src/BST/BTInorder.cpp
+++ src/BST/BTInorder.cpp
@@ -1,4 +1,18 @@
- void inOrderrecur(TreeNode* root, vector<int>&result) {
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) {
+    val=x;
+    left=NULL;
+    right=NULL;
+  }
+};
+ 
+void inOrderrecur(TreeNode* root, vector<int>&result) {
         if(root!=NULL) {
             inOrderrecur(root->left,result);
             result.push_back(root->val);
@@ -10,3 +24,17 @@
         inOrderrecur(root,result);
         return result;
     }
+int main() {
+  vector<int> inOrder;
+  TreeNode* root=new TreeNode(1);
+  root->right=new TreeNode(2);
+  root->right->left=new TreeNode(3);  
+  inOrder=inOrderTraversal(root);
+
+  for(int i=0;i<inOrder.size();i++) {
+    cout<<" " <<inOrder[i];
+  }
+  cout<<"\n\n";
+  return 0;
+  //std::cout << "Hello World!\n";
+}

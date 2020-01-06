92092e5753c17825d92deb01f6f33f3733582880
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2020-01-05 22:54:58 -0600

    Update BTpreOrderIter.cpp

--- src/BST/BTpreOrderIter.cpp
+++ src/BST/BTpreOrderIter.cpp
@@ -1,4 +1,18 @@
 
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) {
+    val=x;
+    left=NULL;
+    right=NULL;
+  }
+};
+
 vector<int> preorderTraversal(TreeNode *root) {
     vector<int> result;
     stack<TreeNode*> myStack;
@@ -19,4 +33,19 @@ vector<int> preorderTraversal(TreeNode *root) {
     }
     
     return result;
+}
+
+int main() {
+  vector<int> preOrder;
+  TreeNode* root=new TreeNode(1);
+  root->right=new TreeNode(2);
+  root->right->left=new TreeNode(3);  
+  preOrder=preOrderTraversal(root);
+
+  for(int i=0;i<preOrder.size();i++) {
+    cout<<" " <<preOrder[i];
+  }
+  cout<<"\n\n";
+  return 0;
+  //std::cout << "Hello World!\n";
 }

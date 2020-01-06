33febc155f98fa9f53521aff8cc679c1c76130e9
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2020-01-06 07:11:52 -0600

    Update BTlevelOrderTraversal.cpp

--- src/BST/BTlevelOrderTraversal.cpp
+++ src/BST/BTlevelOrderTraversal.cpp
@@ -1,4 +1,18 @@
-vector<vector<int>> levelOrder(TreeNode* root) {
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) {
+    val=x;
+    left=NULL;
+    right=NULL;
+  }
+};
+
+vector<vector<int>> levelOrderTraversal(TreeNode* root) {
         queue<TreeNode*> Q;
         vector<vector<int>>result;
         if(root==NULL) {
@@ -38,3 +52,22 @@ vector<vector<int>> levelOrder(TreeNode* root) {
         result.push_back(op);
         return result;
     }
+
+int main() {
+  vector<vector<int>> levelOrder;
+  TreeNode* root=new TreeNode(1);
+  root->right=new TreeNode(2);
+  root->right->left=new TreeNode(3);  
+  levelOrder=levelOrderTraversal(root);
+
+  for(int i=0;i<levelOrder.size();i++) {
+    vector<int>temp=levelOrder[i];
+          for(int j=0;j<temp.size();j++) {
+                  cout<<temp[j]<<" ";
+          }
+          cout<<"\n";
+  }
+  cout<<"\n\n";
+  return 0;
+  //std::cout << "Hello World!\n";
+}

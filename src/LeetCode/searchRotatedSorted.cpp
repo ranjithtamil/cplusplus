5065a71625c6cac5acb7373a6bb880f573152e9e
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2020-12-07 16:02:11 -0600

    Create searchRotatedSorted.cpp

---
+++ src/LeetCode/searchRotatedSorted.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0;
+        int n=nums.size();
+        int end=n-1;
+        while(start<=end) {
+            int mid=(start)+(end-start)/2;
+            if(target == nums[mid]) {
+                return mid;
+            }
+            if(nums[mid] >= nums[start]) {
+                if(target >= nums[start] && target < nums[mid]) {
+                    end = mid-1;
+                }
+                else {
+                    start=mid+1;
+                }
+            }
+            else {
+                if(target <= nums[end] && target > nums[mid]) {
+                    start=mid+1;
+                }
+                else {
+                    end=mid-1;
+                }
+            }
+        }
+        return -1;
+    } 
+    };

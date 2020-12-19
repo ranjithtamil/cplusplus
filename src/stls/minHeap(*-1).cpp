62d8f504b0892c7be0f7dc9c13c8be75622c7192
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2020-12-19 09:03:43 -0600

    Create minHeap(*-1).cpp

---
+++ src/stls/minHeap(*-1).cpp
@@ -0,0 +1,33 @@
+// C++ Progrma to implement min heap
+// using default priority_queue(max-heap)
+ 
+#include <iostream>
+#include <queue>
+using namespace std;
+ 
+int main()
+{
+    // data
+    int arr[] = { 25, 7, 9, 15, 20, 36, 50 };
+   
+      // default priority_queue using max-heap
+    priority_queue<int> pq;
+   
+      // size of the array
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    // multiply -1 with all elements while
+    // inserting
+    for (int i = 0; i < n; i++) {
+        pq.push((-1) * arr[i]);
+    }
+ 
+    // multiply all elements with -1 while
+    // retrive the elements
+    while (!pq.empty()) {
+        cout << (pq.top()) * (-1) << " ";
+        pq.pop();
+    }
+ 
+    return 0;
+}

44c2e90b03c0aad641358bffd66f8b97131ef33c
Ranjith <44685527+ranjithtamil@users.noreply.github.com>
2021-01-01 22:19:59 -0600

    Update houseRobber.cpp

--- src/LeetCode/houseRobber.cpp
+++ src/LeetCode/houseRobber.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        
+        /*
         int incl=0,excl=0;
         for(int i=0;i<nums.size();i++) {
             int temp=incl;
             incl=max(incl,nums[i]+excl);
             excl=max(temp,excl);
         }
+        
         return max(incl,excl);
+        */
         /*
         int n=nums.size();
 	    if(n==0) {
@@ -23,5 +27,42 @@ public:
 	    }
 	    return max(exclusive[n-1],inclusive[n-1]);	
         */
+        
+        
+        //Below solution handles negatives as well
+        int n=nums.size();
+        
+        if(n==0) {
+            return 0;
+        }
+        if(n==1) {
+            return nums[0];
+        }
+        if(n==2) {
+            return max(nums[0],nums[1]);
+        }
+        
+        
+        int second=nums[0];
+        int first=nums[1];
+        int current=nums[2];
+        for(int i=2;i<n;i++) {
+            current = maxmum(nums[i],nums[i]+second,first,second);
+            second = max(first,second);
+            first = current;
+        }
+        return current;
+    }
+    int maxmum(int a,int b, int c,int d) {
+        if(a>=b && a>=c && a>=d) {
+            return a;
+        }
+        if(b>=c && b>=a && b>=d) {
+            return b;
+        }
+        if(c>=a && c>=b && c>=d) {
+            return c;
+        }
+        return d;
     }
 };
